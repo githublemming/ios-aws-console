41cbab4767f703a0bc60aa5a8faeef4130e6649a
markhaskins <markhaskins@mac.com>
2018-12-31 21:39:51 +0000

    added

---
+++ ios-aws-console/Core Data/Objects/EC2.swift
@@ -0,0 +1,55 @@
+//
+//  EC2.swift
+//  ios-aws-consoleTests
+//
+//  Created by Mark Haskins on 31/12/2018.
+//  Copyright © 2018 Mark Haskins. All rights reserved.
+//
+
+import CoreData
+import SwiftyXMLParser
+
+class EC2: NSManagedObject {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<EC2> {
+        return NSFetchRequest<EC2>(entityName: "EC2")
+    }
+
+    @NSManaged public var id: String
+    @NSManaged public var region: String
+    @NSManaged public var xml: String
+
+    var instanceDetail = [(name: String, value: String)]()
+
+    func inflateMinimum() {
+
+        instanceDetail = [(name: String, value: String)]()
+
+        let xmlData = try! XML.parse(xml)
+        instanceDetail[0] = (name: "Instance Id", value: xmlData["item", "instanceId"].text!)
+        instanceDetail[2] = (name: "Status", value: xmlData["item", "instanceState", "name"].text!)
+
+    }
+
+    func inflate() {
+
+        instanceDetail = [(name: String, value: String)]()
+
+        let xmlData = try! XML.parse(xml)
+        instanceDetail[0] = (name: "Instance Id", value: xmlData["item", "instanceId"].text!)
+        instanceDetail[1] = (name: "Image Id", value: xmlData["item", "imageId"].text!)
+        instanceDetail[2] = (name: "Status", value: xmlData["item", "instanceState", "name"].text!)
+    }
+
+    func deflate() {
+        instanceDetail = [(name: String, value: String)]()
+    }
+
+    func instanceId() -> String {
+        return instanceDetail[0].value
+    }
+
+    func instanceState() -> String {
+        return instanceDetail[2].value
+    }
+}
